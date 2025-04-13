a4cd47d3b1f32e99638c969e40b312870f92352d
Andrey Hutornoy <hutornoyaa@gmail.com>
2025-04-13 07:49:35 +0300

    add allowing security

--- app/main.py
+++ app/main.py
@@ -1,25 +1,17 @@
 from fastapi import FastAPI
 from app.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Universal Code Analyzer", version="1.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 app.include_router(router)
 
 
-# Synchronous Example
-from mistralai import Mistral
-import os
 
 
-with Mistral(
-    api_key=os.getenv("MISTRAL_API_KEY", ""),
-) as mistral:
-
-    res = mistral.chat.complete(model="mistral-small-latest", messages=[
-        {
-            "content": "Who is the best French painter? Answer in one short sentence.",
-            "role": "user",
-        },
-    ])
-
-    # Handle response
-    print(res)
